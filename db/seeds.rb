63b9deb403f465564e14a4e76643991233ff6cb2
Heninapso <heninapso@gmail.com>
2017-03-08 11:05:33 +0100

    setup and seeds, to be continued

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  articles = Article.create([{ title: Faker::Lorem.words(6)}, { content: Faker::Lorem.paragraphs(1)}])
+end
